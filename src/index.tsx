852773d38374ef10d237b1e45a758737835942fc
HassanKhan123 <hk2602809@gmail.com>
2021-08-09 12:48:52 +0500

    resetting the iframe content

--- src/index.tsx
+++ src/index.tsx
@@ -26,6 +26,8 @@ const App = () => {
   const submitHandler = async () => {
     if (!ref.current) return;
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
