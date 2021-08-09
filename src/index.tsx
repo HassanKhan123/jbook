fd1356a61443518462198b0b2d5be59eca3d2d07
HassanKhan123 <hk2602809@gmail.com>
2021-08-09 13:20:46 +0500

    displaying editor

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import * as esbuild from 'esbuild-wasm';
 
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
   const [input, setInput] = useState('');
@@ -66,6 +67,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor />
       <textarea
         value={input}
         onChange={e => setInput(e.target.value)}
