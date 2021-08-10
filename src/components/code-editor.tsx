9ebae169b5bbb0b19e4cbdf62d0d011eb75d3cce
HassanKhan123 <hk2602809@gmail.com>
2021-08-10 08:38:10 +0500

    code formatting added

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import MonacoEditor, { OnChange, OnMount } from '@monaco-editor/react';
 import prettier from 'prettier';
 import parser from 'prettier/parser-babel';
@@ -7,15 +8,29 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>();
+
   const handleEditorChange: OnChange = (value, event) => {
     onChange(value);
   };
 
   const handleEditorOnMount: OnMount = (editor, monaco) => {
+    editorRef.current = editor;
     editor.getModel()?.updateOptions({ tabSize: 2 });
   };
 
-  const onFormatClick = () => {};
+  const onFormatClick = () => {
+    const unformatted = editorRef.current.getModel().getValue();
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+
+    editorRef.current.setValue(formatted);
+  };
 
   return (
     <div>
