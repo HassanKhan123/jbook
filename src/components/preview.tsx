0fda2eb052bb7e8a51f141202e53157ae654ed48
HassanKhan123 <hk2602809@gmail.com>
2021-08-11 08:12:25 +0500

    preview component added

---
+++ src/components/preview.tsx
@@ -0,0 +1,47 @@
+import React, { useRef, useEffect } from 'react';
+
+interface PreviewProps {
+  code: string;
+}
+
+const html = `
+<html>
+<head>
+</head>
+<body>
+ <div id="root"></div>
+ <script>
+   window.addEventListener('message',(even) => {
+     try{
+       eval(event.data)
+     }catch(err){
+       const root = document.querySelector('#root')
+       root.innerHTML='<div style="color:red;"><h4>Runtime Error</h4>'+err+'</div>'
+       console.error(err)
+
+     }
+   },false)
+ </script>
+</body>
+</html>
+`;
+
+const Preview: React.FC<PreviewProps> = ({ code }) => {
+  const iframe = useRef<any>();
+
+  useEffect(() => {
+    iframe.current.srcdoc = html;
+    iframe.current.contentWindow.postMessage(code, '*');
+  }, [code]);
+
+  return (
+    <iframe
+      srcDoc={html}
+      sandbox='allow-scripts'
+      ref={iframe}
+      title='preview'
+    />
+  );
+};
+
+export default Preview;
--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,13 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
 
 const App = () => {
+  const [code, setCode] = useState('');
   const [input, setInput] = useState<string | undefined>('');
 
   const ref = useRef<any>();
-  const iframe = useRef<any>();
 
   useEffect(() => {
     startService();
@@ -27,8 +28,6 @@ const App = () => {
   const submitHandler = async () => {
     if (!ref.current) return;
 
-    iframe.current.srcdoc = html;
-
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -40,31 +39,8 @@ const App = () => {
       },
     });
 
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    setCode(result.outputFiles[0].text);
   };
-
-  const html = `
-   <html>
-   <head>
-   </head>
-   <body>
-    <div id="root"></div>
-    <script>
-      window.addEventListener('message',(even) => {
-        try{
-          eval(event.data)
-        }catch(err){
-          const root = document.querySelector('#root')
-          root.innerHTML='<div style="color:red;"><h4>Runtime Error</h4>'+err+'</div>'
-          console.error(err)
-
-        }
-      },false)
-    </script>
-   </body>
-   </html>
-  `;
 
   return (
     <div>
@@ -72,20 +48,11 @@ const App = () => {
         initialValue='const b = 1 ;'
         onChange={val => setInput(val)}
       />
-      <textarea
-        value={input}
-        onChange={e => setInput(e.target.value)}
-      ></textarea>
       <div>
         <button onClick={submitHandler}>Submit</button>
       </div>
 
-      <iframe
-        srcDoc={html}
-        sandbox='allow-scripts'
-        ref={iframe}
-        title='preview'
-      />
+      <Preview code={code} />
     </div>
   );
 };
