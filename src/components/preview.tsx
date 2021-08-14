f9b98fcb774bce583f0505f02bbdc757bca00f3b
HassanKhan123 <hk2602809@gmail.com>
2021-08-14 15:03:20 +0500

    handled async errors

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -18,16 +18,24 @@ const html = `
 <body>
  <div id="root"></div>
  <script>
-   window.addEventListener('message',(even) => {
-     try{
-       eval(event.data)
-     }catch(err){
-       const root = document.querySelector('#root')
-       root.innerHTML='<div style="color:red;"><h4>Runtime Error</h4>'+err+'</div>'
-       console.error(err)
+    const handleError = (err) => {
+      const root = document.querySelector('#root')
+      root.innerHTML='<div style="color:red;"><h4>Runtime Error</h4>'+err+'</div>'
+      console.error(err)
+    }
 
-     }
-   },false)
+    window.addEventListener('error',(event) => {
+      event.preventDefault()
+      handleError(event.error)
+    })
+
+    window.addEventListener('message',(event) => {
+      try{
+        eval(event.data)
+      }catch(err){
+        handleError(err)
+      }
+    },false)
  </script>
 </body>
 </html>
