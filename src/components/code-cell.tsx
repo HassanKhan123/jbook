cf37c3036beeb3fd028d92b48bbb3a117b53fd83
HassanKhan123 <hk2602809@gmail.com>
2021-08-14 13:35:01 +0500

    added: timeout for code execution

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import CodeEditor from './code-editor';
 import Preview from './preview';
@@ -9,10 +9,16 @@ const CodeCell = () => {
   const [code, setCode] = useState('');
   const [input, setInput] = useState<string | undefined>('');
 
-  const submitHandler = async () => {
-    const output = await bundler(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundler(input);
+      setCode(output);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
     <Resizable direction='vertical'>
